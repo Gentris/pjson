78eb29f0f0ce6e0099368e345e57075a0a00c304
Gentris Leci <gentris@viridios.ai>
2023-11-09 10:27:57 +0100

    Parse as string when unable to parse

--- pjson.py
+++ pjson.py
@@ -1,13 +1,14 @@
-#! /usr/bin/python3
+#!/usr/bin/python3
+
+import ast
 import json
 import sys
 
-stdin = ''.join(sys.stdin.readlines())
-
+stdin: str = "".join(sys.stdin.readlines())
 try:
-   json_str = json.loads(stdin)
-except Exception as ex:
-    raise Exception('Unable to parse stdin, make sure to use JSON only') from ex
+    parsed = ast.literal_eval(stdin)
+except ValueError:
+    parsed = str(stdin)
+stdout: str = json.dumps(parsed, indent=4, default=str)
 
-print(json.dumps(json_str, indent=4, default=str))
-
+print(stdout)
